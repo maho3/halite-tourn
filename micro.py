ded121607e6bbc6be5424f429e665732e9416d21
Scott Sussex <scott@Scotts-MacBook-Pro-2.local>
2018-03-16 13:04:55 -0400

    getMoves update

--- micro.py
+++ micro.py
@@ -1,11 +1,16 @@
+import hlt
+
 def getMovesForObjective(objective, game_map, own_ships_nav):
     commands = []
     for ship in objective.my_ships:
-        command = navigate(navigate_command = ship.navigate(
-                                                ship.closest_point_to(objective.entity),
-                                                game_map,
-                                                speed=int(hlt.constants.MAX_SPEED),
-                                                entities=own_ships_nav)
+        if objective.objtype == dock and ship.can_dock(objective.entity):
+            command = (ship.dock(objective.entity))
+        else:
+            command = navigate(navigate_command = ship.navigate(
+                                                    ship.closest_point_to(objective.entity),
+                                                    game_map,
+                                                    speed=int(hlt.constants.MAX_SPEED),
+                                                    entities=own_ships_nav)
         if (command != None):
             commands.append(command)
     return commands
